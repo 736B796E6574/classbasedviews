5501651038633e850715f7cae2ba57ce2cbf5fa9
martin-mcinerney <martin_mcinerney@live.ie>
2023-02-12 21:05:53 +0000

    Adds admin button to list view

--- cbviews/urls.py
+++ cbviews/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', PostList.as_view(), name='my_list_view'),
     path('update/<int:pk>/', PostUpdateView.as_view(), name='my_update_view'),
     path('create/', PostCreateView.as_view(), name='create_view'),
-    path('delete/<int:pk>/', PostDeleteView.as_view(), name='delete_view')
+    path('delete/<int:pk>/', PostDeleteView.as_view(), name='delete_view'),
+    path('create/', PostCreateView.as_view(), name='create_view'),
 ]
-
